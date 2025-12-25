9db389c420c5492ec3d57433528949225739672d
Alex Yaroshuk <alexander_4288@Yahoo.com>
2025-12-25 22:22:58 +0800

    ci: update vercel.json

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Installing Flutter..."
+cd /tmp
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+export PATH="/tmp/flutter/bin:$PATH"
+
+echo "Verifying Flutter installation..."
+flutter --version
+
+echo "Navigating to project directory..."
+cd $VERCEL_PROJECT_DIR
+
+echo "Getting dependencies..."
+flutter pub get
+
+echo "Building for web..."
+flutter build web --release
+
+echo "Build complete!"
