5436f1de6e03910ad8f4096039faeb41c80c95cb
Alex Yaroshuk <alexander_4288@Yahoo.com>
2025-12-25 22:33:29 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Store the project directory before we cd
+PROJECT_DIR="$(pwd)"
+
 echo "Installing Flutter..."
 cd /tmp
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable
@@ -9,7 +12,9 @@ export PATH="/tmp/flutter/bin:$PATH"
 echo "Verifying Flutter installation..."
 flutter --version
 
-echo "Current directory: $(pwd)"
+echo "Returning to project directory: $PROJECT_DIR"
+cd "$PROJECT_DIR"
+
 echo "Getting dependencies..."
 flutter pub get
 
